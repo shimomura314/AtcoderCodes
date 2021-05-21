e3ce2862715e537988fcb0e348f6326aa91964f1
shimomura314 <shimomura.yuki.ut@gmail.com>
2021-05-21 18:39:30 +0900

    for ABC under 42

--- scraping.py
+++ scraping.py
@@ -9,11 +9,21 @@ file_path = os.environ["contest"]
 dirpath = file_path + "\\testcase"
 contest = file_path[-6:].lower()
 
+
+def save_file(file_path: str, data: str) -> None:
+    with open(file_path, "w") as _file:
+        _file.write(data)
+    return
+
+
 def scraping():
     contest_type = contest[:3] 
     contest_number = int(contest[3:])
 
-    if (contest_type == "abc" and 126 <= contest_number) or contest_type == "agc" or (contest_type == "arc" and 104 <= contest_number):
+    # URL setting be contest
+    if contest_type == "abc" and contest_number < 20:
+        difficulties = ["1", "2", "3", "4"]
+    elif (contest_type == "abc" and 126 <= contest_number) or contest_type == "agc" or (contest_type == "arc" and 104 <= contest_number):
         difficulties = ["a", "b", "c", "d", "e", "f"]
     else:
         difficulties = ["a", "b", "c", "d"]
@@ -29,6 +39,7 @@ def scraping():
     for i in range(len(mixed_ABC_number)):
         mixed_ABC[ mixed_ABC_number[i] ] = "arc%03d" %(100-i)
 
+    # Get data by difficulties
     for difficulty in difficulties:
         if contest_type == "abc" and (contest_number in mixed_ABC_number) and (difficulty == "c" or difficulty == "d"):
             if difficulty == "c":
@@ -39,40 +50,41 @@ def scraping():
             problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + contest +  "_" + difficulty
         problem_html = requests.get(problem_url)
         problem_soup = BeautifulSoup(problem_html.content, "html.parser")
-        testcase = []
-        tags =  problem_soup.find_all("pre")
-        for f in tags:
-            testcase.append(f.get_text())
+        
+        if contest_type == "abc" and contest_number <= 41:
+            testcase = []
+            tags = problem_soup.find_all("pre")
+            for f in tags:
+                temp = []
+                for e in f.get_text().split("\n"):
+                    if e != "r":
+                        temp.append(e.strip("\r"))
+                temp = [element for element in temp if element != ""]
+                temp = "\n".join(temp) + "\n"
+                testcase.append(temp)
 
-        n = len(testcase)//2
-        if n%2:
-            for i in range(1, n, 2):
+            for i in range(1, len(testcase)):
                 case_number = (i + 1) // 2
-
-                in_path = dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt"
-                f_in = open(in_path, "w")
-                testcase[i] = testcase[i].replace("\n", "")
-                f_in.write(testcase[i])
-                f_in.close()
-
-                out_path = dirpath + "/output_" + difficulty.upper() + str(case_number) + ".txt"
-                f_out = open(out_path, "w")
-                testcase[i+1] = testcase[i+1].replace("\n", "")
-                f_out.write(testcase[i + 1])
-                f_out.close()
+                if i%2 == 1:
+                    save_file(dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt", testcase[i])
+                else:
+                    save_file(dirpath + "/output_" + difficulty.upper() + str(case_number) + ".txt", testcase[i])
         else:
-            for i in range(2, n, 2):
-                case_number = (i + 1) // 2
 
-                in_path = dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt"
-                f_in = open(in_path, "w")
-                testcase[i] = testcase[i].replace("\n", "")
-                f_in.write(testcase[i])
-                f_in.close()
+            testcase = []
+            tags =  problem_soup.find_all("pre")
+            for f in tags:
+                testcase.append(f.get_text())
 
-                out_path = dirpath + "/output_" + difficulty.upper() + str(case_number) + ".txt"
-                f_out = open(out_path, "w")
-                testcase[i+1] = testcase[i+1].replace("\n", "")
-                f_out.write(testcase[i + 1])
-                f_out.close()
+            n = len(testcase)//2
+            if n%2:
+                for i in range(1, n, 2):
+                    case_number = (i + 1) // 2
+                    save_file(dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt", testcase[i].replace("\n", ""))
+                    save_file(dirpath + "/output_" + difficulty.upper() + str(case_number) + ".txt", testcase[i+1].replace("\n", ""))
+            else:
+                for i in range(2, n, 2):
+                    case_number = (i + 1) // 2
+                    save_file(dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt", testcase[i].replace("\n", ""))
+                    save_file(dirpath + "/output_" + difficulty.upper() + str(case_number) + ".txt", testcase[i+1].replace("\n", ""))
     return
