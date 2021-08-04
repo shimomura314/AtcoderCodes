55228b0441fb3e4c4b141a59fb8d08d31f873f70
shimomura314 <shimomura.yuki.ut@gmail.com>
2021-08-04 11:56:52 +0900

    ABC178 ABC

--- ABC178/C.py
+++ ABC178/C.py
@@ -1,6 +1,13 @@
-mod = 10**9
+mod = 10**9 + 7
 n = int(input())
-answer = pow(10, n-2, mod)
-answer *= n*(n-1)//2
-answer %= mod
-print(answer)
+include_none = 1
+include_or = 0
+include_and = 0
+for _ in range(n):
+    include_and = include_and*10 + include_or
+    include_or = include_or*9 + include_none*2
+    include_none = include_none * 8
+    include_and %= mod
+    include_or %= mod
+    include_none %= mod
+print(include_and)
