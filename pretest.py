36aed1d5eb0adbe016f7ad0117a97145c868e72e
shimomura314 <shimomura.yuki.ut@gmail.com>
2021-11-01 12:46:47 +0900

    add new ABCs

--- pretest.py
+++ pretest.py
@@ -11,25 +11,26 @@ problem = os.environ["problem"]
 contest = os.environ["contest"]
 dirpath = contest + "\\testcase"
 
+
 def main():
     # make a directory of testcases
     if not os.path.exists(dirpath): 
         os.makedirs(dirpath)
         scraping()
-    
+
     number_of_probrems = 0
     for number in range(1, 100):
         if os.path.isfile(dirpath + "/input_" + problem + str(number) + ".txt"):
             number_of_probrems += 1
         else:
             break
-    
+
     is_correct = True
-    for number in range(1, number_of_probrems + 1): # number of testcases
+    for number in range(1, number_of_probrems + 1):  # number of testcases
         in_path = dirpath + "/input_" + problem + str(number) + ".txt"
         out_path = dirpath + "/output_" + problem + str(number) + ".txt"
         res_path = dirpath + "/response_" + problem + str(number) + ".txt"
-        
+
         # execute the program
         f = open(in_path)
         data = f.read()
@@ -37,7 +38,7 @@ def main():
         with open(in_path) as fin:
             with open(res_path, "w") as fres:
                 subprocess.run("python " + contest + "\\" + problem + ".py", stdin=fin, stdout=fres, timeout=TLE, shell = True)
-        
+
         # check the response
         f_out = open(out_path, "r")
         out_str = "".join(f_out.read().splitlines())
--- scraping.py
+++ scraping.py
@@ -17,40 +17,42 @@ def save_file(file_path: str, data: str) -> None:
 
 
 def scraping():
-    contest_type = contest[:3] 
+    contest_type = contest[:3]
     contest_number = int(contest[3:])
 
     # URL setting be contest
-    if (contest_type == "abc" and 126 <= contest_number) or contest_type == "agc" or (contest_type == "arc" and 104 <= contest_number):
+    if contest_type == "abc" and 212 <= contest_number:
+        difficulties = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    elif (contest_type == "abc" and 126 <= contest_number) or contest_type == "agc" or (contest_type == "arc" and 104 <= contest_number):
         difficulties = ["a", "b", "c", "d", "e", "f"]
     else:
         difficulties = ["a", "b", "c", "d"]
-    
+
     mixed_ABC_number = [
-        102, 101, 98, 97, 95, 94, 93, 92, 91, 90, 
-        87, 86, 83, 82, 81, 78, 77, 74, 72, 71, 
-        69, 68, 67, 66, 65, 63, 62, 60, 59, 58, 
-        56, 55, 53, 52, 50, 49, 48, 47, 46, 45, 
-        44, 43, 42
+        102, 101, 98, 97, 95, 94, 93, 92, 91, 90,
+        87, 86, 83, 82, 81, 78, 77, 74, 72, 71,
+        69, 68, 67, 66, 65, 63, 62, 60, 59, 58,
+        56, 55, 53, 52, 50, 49, 48, 47, 46, 45,
+        44, 43, 42,
         ]
     mixed_ABC = {}
     for i in range(len(mixed_ABC_number)):
-        mixed_ABC[ mixed_ABC_number[i] ] = "arc%03d" %(100-i)
+        mixed_ABC[mixed_ABC_number[i]] = "arc%03d" % (100-i)
 
     # Get data by difficulties
     for difficulty in difficulties:
         if contest_type == "abc" and (contest_number in mixed_ABC_number) and (difficulty == "c" or difficulty == "d"):
             if difficulty == "c":
-                problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + mixed_ABC[contest_number] +  "_a"
+                problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + mixed_ABC[contest_number] + "_a"
             if difficulty == "d":
-                problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + mixed_ABC[contest_number] +  "_b"
+                problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + mixed_ABC[contest_number] + "_b"
         elif contest_type == "abc" and contest_number < 20:
-            problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + contest +  "_" + str(ord(difficulty)-96)
+            problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + contest + "_" + str(ord(difficulty)-96)
         else:
-            problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + contest +  "_" + difficulty
+            problem_url = "https://atcoder.jp/contests/" + contest + "/tasks/" + contest + "_" + difficulty
         problem_html = requests.get(problem_url)
         problem_soup = BeautifulSoup(problem_html.content, "html.parser")
-        
+
         if contest_type == "abc" and contest_number <= 41:
             testcase = []
             tags = problem_soup.find_all("pre")
@@ -65,19 +67,19 @@ def scraping():
 
             for i in range(1, len(testcase)):
                 case_number = (i + 1) // 2
-                if i%2 == 1:
+                if i % 2 == 1:
                     save_file(dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt", testcase[i])
                 else:
                     save_file(dirpath + "/output_" + difficulty.upper() + str(case_number) + ".txt", testcase[i])
         else:
 
             testcase = []
-            tags =  problem_soup.find_all("pre")
+            tags = problem_soup.find_all("pre")
             for f in tags:
                 testcase.append(f.get_text())
 
             n = len(testcase)//2
-            if n%2:
+            if n % 2:
                 for i in range(1, n, 2):
                     case_number = (i + 1) // 2
                     save_file(dirpath + "/input_" + difficulty.upper() + str(case_number) + ".txt", testcase[i].replace("\n", ""))
